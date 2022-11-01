d1f914cccd40e3564459f15f76da4a3d59ffc4d7
Khaug <huynhkhang690@gmail.com>
2022-11-01 11:32:58 +0700

    complete menu and fix min-width:2000px in navbar&aboutus

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => (
     <Navbar />
     <Header />
     <AboutUs />
-    {/* <SpecialMenu />
-    <Chef />
+    <SpecialMenu />
+    {/* <Chef />
     <Intro />
     <Laurels />
     <Gallery />
